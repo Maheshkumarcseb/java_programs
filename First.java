fa72386a71b48ab46da3f013b6ac2b1a8ec98567
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-12-23 14:56:19 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args)
+    {
+        System.out.println("what is your name");
+        System.out.println("my first java progam");
+    }
+    
+}
