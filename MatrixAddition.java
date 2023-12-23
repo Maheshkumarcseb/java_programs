6be4dd39219b55d8f7a03110ee4658b2ea7b74d5
Maheshkumarcseb <141344157+Maheshkumarcseb@users.noreply.github.com>
2023-12-24 02:37:38 +0530

    MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -1,70 +1,60 @@
-import java.util.Scanner;
-
-
-public class MatrixAddition {
-    public static void main(String[] args) {
-        if (args.length != 1) {
+import java.util.Scanner;    // for making object of the class
+public class MatrixAddition {    
+    public static void main(String[] args) {    //main method
+        if (args.length != 1) {       //checking the length of the string.
              System.out.println("please provide a single integer");
             return;
         }
-        int N = Integer.parseInt(args[0]);
-        if (N <= 0) {
+        int N = Integer.parseInt(args[0]);   // converting string datatype to integer datatype using parseInt().
+        if (N <= 0) {   
             System.out.println("N must be a positive number.");
         }
-        Scanner scanner = new Scanner(System.in);
-        int[][] matrix1 = new int[N][N];
+        Scanner scanner = new Scanner(System.in);   //declaring scanner object for the class. 
+        int[][] matrix1 = new int[N][N];    
         int[][] matrix2 = new int[N][N];
-
         System.out.println("enter value for matrix 1");
-
-        readMatrixValues(matrix1, scanner);
-
+        readMatrixValues(matrix1, scanner);   //calling read method for matrix 1.
         System.out.println("enter the value for matrix 2");
-        readMatrixValues(matrix2, scanner);
-
+        readMatrixValues(matrix2, scanner);   // //calling read method for matrix 2.
         System.out.println("matrix 1:");
-        printMatrix(matrix1);
-
+        printMatrix(matrix1);  //printing the value of matrix1.
         System.out.println("matrix 2:");
-        printMatrix(matrix2);
-
+        printMatrix(matrix2);   //printing the value of matrix2.
         int[][] result = addMatrices(matrix1, matrix2);
         System.out.println("sum of two matrices");
-
-        printMatrix(result);
-
-        scanner.close();
+        printMatrix(result);   //calling the printMatrix method.
+        scanner.close();    // closing the scanner class.
     }
 
     public static void readMatrixValues(int[][] matrix, Scanner scanner) {
-        int N = matrix.length;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        int N = matrix.length;    //storing the matrix length into a variable N.
+        for (int i = 0; i < N; i++) {   // this loop for rows
+            for (int j = 0; j < N; j++) {    // this loop for columns
                 System.out.println("enter the value for rows" + (i + 1) + "columns" + (j + 1) + ":");
-                matrix[i][j] = scanner.nextInt();
+                matrix[i][j] = scanner.nextInt();   // taking the element of matrix from user
             }
         }
     }
 
     public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
-        int N = matrix1.length;
+        int N = matrix1.length;   //storing the matrix length into a variable N.
         int[][] result = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                result[i][j] = matrix1[i][j] + matrix2[i][j];
+                result[i][j] = matrix1[i][j] + matrix2[i][j];   //adding both the matrix
             }
         }
         return result;
     }
 
-    public static void printMatrix(int[][] matrix) {
-        int N = matrix.length;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.println(matrix[i][j] + " ");
+    public static void printMatrix(int[][] matrix) {   // method for printing the addition of two matrix.
+        int N = matrix.length;    //storing the matrix length into a variable N.
+        for (int i = 0; i < N; i++) {     // this loop for rows
+            for (int j = 0; j < N; j++) {   // this loop for columns
+                System.out.println(matrix[i][j] + " ");  
             }
-            System.out.println();
+            System.out.println(); // printing a new line
         }
     }
 }
