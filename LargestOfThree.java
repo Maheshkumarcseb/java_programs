87bea6b0d25117bd0feea2391e91b2de744868b7
Maheshkumarcseb <141344157+Maheshkumarcseb@users.noreply.github.com>
2023-12-24 02:20:13 +0530

    LargestOfThree.java

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -1,20 +1,17 @@
 
-import java.util.Scanner;
-
-public class LargestOfThree {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+import java.util.Scanner;  // for using nextInt()
+public class LargestOfThree {   // method of the class
+    public static void main(String[] args) {    
+        Scanner scanner = new Scanner(System.in);   // creating a object named scanner
         System.out.println("Enter three numbers:");
+        int num1 = scanner.nextInt();  // taking integer input from user with the help of scanner object
+        int num2 = scanner.nextInt();  // taking integer input from user with the help of scanner object
+        int num3 = scanner.nextInt();  // taking integer input from user with the help of scanner object 
 
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
+        int largest = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3); // finding the greatest of three number using ternary operator.
 
-        int largest = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+        System.out.println("The largest number is: " + largest);  //printing the largest number
 
-        System.out.println("The largest number is: " + largest);
-
-        scanner.close();
+        scanner.close(); //closing the scanner class
 }
 }
