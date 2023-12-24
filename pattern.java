3894215f338e8d4281b04d517da223a112a1997f
Maheshkumarcseb <141344157+Maheshkumarcseb@users.noreply.github.com>
2023-12-24 14:55:38 +0530

    pattern.java

--- pattern.java
+++ pattern.java
@@ -1,15 +1,15 @@
 public class pattern {
-    public static void main(String[] args) {
-        three:
-        for(int i=0;i<5;i++)
+    public static void main(String[] args) {   //main method of class
+        three:          //labelled break
+        for(int i=0;i<5;i++)  // outer loop
         {
-            for(int j=0;j<5;j++)
+            for(int j=0;j<5;j++)  // inner loop
             {
-                if(i==3)
-                 break three;
+                if(i==3)       // condition for labelled break
+                 break three;     // it will come out of both loop
                System.out.print("*");
             }
-           System.out.println();
+           System.out.println(); 
         }
         
     }
